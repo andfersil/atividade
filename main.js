cfd89d295bacc33af5f8107e1f821089a2cf041b
andfersil <142528127+andfersil@users.noreply.github.com>
2024-09-27 11:56:42 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 
 
 function  areQuadraado(lado) {
-return lado**2
-}
-console.log (areQuadraado(4,4) );
+    return lado**2
+    }
+    console.log (areQuadraado(4,4) );
+
+
+    let media=function (num1,num2,num3,num4){
+        return num1+num2+num3/num4;
+    
+    }
+    console.log(1,2,3,3);
+        
+
+
+
+
+
+
+
 
