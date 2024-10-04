1b8d0d5e91fb2eb5c6e68932715a35949e7712d7
andfersil <142528127+andfersil@users.noreply.github.com>
2024-10-04 11:51:29 -0300

    ..

--- main.js
+++ main.js
@@ -19,8 +19,8 @@
 // EXEMPLO DE LISTA let status = ['Wesley', 'André', 'Gabriel', 'Matheus', 'Michel', 'João'];
 
 
-// let estados = ['PR', 'SC', 'RS', 'SP', 'MA','BA','AC','SE','AL','PE', 'MT', 'MS', 'GO', 'MG','ES','RJ','PA','RR','RO','AM','AP','CE','DF','TO','PI','PB','RN'];
-// estados = estados.sort();
+ let estados = ['PR', 'SC', 'RS', 'SP', 'MA','BA','AC','SE','AL','PE', 'MT', 'MS', 'GO', 'MG','ES','RJ','PA','RR','RO','AM','AP','CE','DF','TO','PI','PB','RN'];
+estados = estados.sort();
 // console.log(estados);
 
 let i = 0;
