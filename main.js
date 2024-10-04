24e64edbe087c32619aaedcad11e731fc98148f8
andfersil <142528127+andfersil@users.noreply.github.com>
2024-10-04 11:32:17 -0300

    ..

--- main.js
+++ main.js
@@ -21,7 +21,7 @@
 
 let estados = ['PR', 'SC', 'RS', 'SP', 'MA','BA','AC','SE','AL','PE', 'MT', 'MS', 'GO', 'MG','ES','RJ','PA','RR','RO','AM','AP','CE','DF','TO','PI','PB','RN'];
 estados = estados.sort();
-console.log(estados[0]);
+console.log(estados);
 
 
 
