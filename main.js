2071b4e5a4c947ead3dd4ecd3fe73c6bc656dfd6
andfersil <142528127+andfersil@users.noreply.github.com>
2024-10-04 11:18:48 -0300

    ..

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 
 
-function  areQuadraado(lado) {
-    return lado**2
-    }
-    console.log (areQuadraado(2,2) );
+// function  areQuadraado(lado) {
+//     return lado**2
+//     }
+//     console.log (areQuadraado(2,2) );
 
 
-    let media=function (num1,num2,num3,){
-        return (num1+num2+num3)/num3;
+//     let media=function (num1,num2,num3,){
+//         return (num1+num2+num3)/num3;
     
-    }
-    console.log(media(1,2,3));
+//     }
+//     console.log(media(1,2,3));
 
 
-    let nome= parseInt(prompt(9+9+0/3));
-    document.write(9+9+0/3);
+//     let nome= parseInt(prompt(9+9+0/3));
+//     document.write(9+9+0/3);
         
+// EXEMPLO DE LISTA let status = ['Wesley', 'André', 'Gabriel', 'Matheus', 'Michel', 'João'];
 
 
+let estados = ['PR', 'SC', 'RS', 'SP', 'MA','BA','AC','SE','AL','PE', 'MT', 'MS', 'GO', 'MG','ES','RJ','PA','RR','RO','AM','AP','CE','DF','TO','PI','PB'];
 
-
+console.log(estados.length)
 
 
 
