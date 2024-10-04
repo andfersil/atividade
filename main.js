2eda5aa6c1ab64025c32d6a614d3fba5d14208b6
andfersil <142528127+andfersil@users.noreply.github.com>
2024-10-04 11:23:36 -0300

    ..

--- main.js
+++ main.js
@@ -20,7 +20,7 @@
 
 
 let estados = ['PR', 'SC', 'RS', 'SP', 'MA','BA','AC','SE','AL','PE', 'MT', 'MS', 'GO', 'MG','ES','RJ','PA','RR','RO','AM','AP','CE','DF','TO','PI','PB','RN'];
-estados = estados.sort
+estados = estados.sort()
 alert(estados)
 
 
